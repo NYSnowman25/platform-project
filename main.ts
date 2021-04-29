b10a9dfc49502d4c9f0f266cc13169f7264fd5a2
NYSnowman25 <80698546+NYSnowman25@users.noreply.github.com>
2021-04-29 11:11:11 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy += -100
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile29`, function (sprite, location) {
+    mySprite.vy += -400
+})
 let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 7 7 7 7 7 . . . . . . 
-    . . . . . 7 . . . 7 . . . . . . 
-    . . . . . 7 . . . 7 . . . . . . 
-    . . . . . 7 . . . 7 . . . . . . 
-    . . . . . 7 7 7 7 7 . . . . . . 
-    . . . . 7 . . 7 . . 7 . . . . . 
-    . . . . . 7 7 7 7 7 . . . . . . 
-    . . . . . . . 7 . . . . . . . . 
-    . . . . . . . 7 . . . . . . . . 
-    . . . . . . 7 . 7 . . . . . . . 
-    . . . . . . 7 . 7 . . . . . . . 
-    . . . . . 7 . . . 7 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . 
+    . . . . . 2 2 2 2 . . . . 
+    . . . . 2 2 e e 2 . . . . 
+    . . . . 2 e e e e . . . . 
+    . . . . 2 e 1 f e e . . . 
+    . . . . . e e e e . . . . 
+    . . . . . e e e e . . . . 
+    . . . . . . e . . . . . . 
+    . . . 4 4 4 e 4 4 4 . . . 
+    . . . 4 4 4 4 4 4 4 . . . 
+    . . . 4 4 4 4 4 4 4 . . . 
+    . . . e 4 4 4 4 4 e . . . 
+    . . . . 4 4 4 4 4 . . . . 
+    . . . . 4 4 . 4 4 . . . . 
+    . . . . 4 4 . 4 4 . . . . 
+    . . . . f f . f f . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 mySprite.setPosition(15, 105)
